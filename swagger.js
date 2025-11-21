cb4b47970f7b25ff166d26e8349558070ad99d11
AsmamawDinku <adinku@byupathway.edu>
2025-11-20 20:04:44 -0500

    URGENT FIX: Swagger production URL configuration

---
+++ swagger.js
@@ -0,0 +1,28 @@
+﻿const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'E-Commerce API',
+      version: '1.0.0',
+      description: 'E-Commerce API with Products and Orders management',
+    },
+    servers: [
+      {
+        url: 'https://ecommerce-api-8ewr.onrender.com',
+        description: 'Production server',
+      },
+      {
+        url: 'http://localhost:3000',
+        description: 'Development server',
+      },
+    ],
+  },
+  apis: ['./routes/*.js'],
+};
+
+const specs = swaggerJsdoc(options);
+
+module.exports = { swaggerUi, specs };
